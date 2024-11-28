b0872df76db01c657e723d188a19ae1ee4ad28e0
Avinoam Nukrai <nukrai.avinoam@mail.huji.ac.il>
2024-11-28 22:17:55 +0200

    final website version2

--- app/nasa/page.tsx
+++ app/nasa/page.tsx
@@ -2,14 +2,23 @@ import styles from "./page.module.css";
 import { getData } from "./utils";
 import { Apod } from "./components/Apod";
 
+// Define the type for each item in the data array
+type ApodItem = {
+  title: string;
+  explanation: string;
+  date: string;
+  url: string;
+};
+
 export default async function NASA() {
-  const data = await getData(20);
+  // Specify that getData returns an array of ApodItem
+  const data: ApodItem[] = await getData(20);
 
   return (
     <main className={styles.container}>
       <h1 className={styles.title}>Welcome to NASA Astronomy Pictures</h1>
       <div className={styles.grid}>
-        {data.map((item, index) => (
+        {data.map((item: ApodItem, index: number) => (
           <Apod
             key={index}
             title={item.title}
